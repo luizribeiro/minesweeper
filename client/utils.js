fd1d3e0f7bc68af958071d4a4b6b100859947467
Luiz Ribeiro <luizribeiro@gmail.com>
2012-04-21 01:07:53 -0300

    Verified utils.js with JSLint

--- client/utils.js
+++ client/utils.js
@@ -1,21 +1,21 @@
-(function () {
-    function relMouseCoords(event) {
-        var totalOffsetX = 0;
-        var totalOffsetY = 0;
-        var canvasX = 0;
-        var canvasY = 0;
-        var currentElement = this;
+var Utils = (function () {
+    'use strict';
+    /*global HTMLCanvasElement */
 
-        do{
+    HTMLCanvasElement.prototype.relMouseCoords = function (event) {
+        var totalOffsetX = 0, totalOffsetY = 0,
+            canvasX = 0, canvasY = 0,
+            currentElement = this;
+
+        do {
             totalOffsetX += currentElement.offsetLeft;
             totalOffsetY += currentElement.offsetTop;
-        } while(currentElement = currentElement.offsetParent)
+            currentElement = currentElement.offsetParent;
+        } while (currentElement);
 
         canvasX = event.pageX - totalOffsetX;
         canvasY = event.pageY - totalOffsetY;
 
-        return { x : canvasX, y : canvasY }
-    }
-
-    HTMLCanvasElement.prototype.relMouseCoords = relMouseCoords;
-})();
+        return { x : canvasX, y : canvasY };
+    };
+}());
